aa6611783a6b0c89e1a33a1cfed2bda85e4ffbf6
SteveJ123 <jjanardhan@gmail.com>
2016-01-13 00:52:58 +0530

    Created  Hello World app

---
+++ app/src/main/java/com/jana/androidlesions/lesson1.java
@@ -0,0 +1,13 @@
+package com.jana.androidlesions;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class lesson1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lesson1);
+    }
+}
